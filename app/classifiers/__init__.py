aa9cad0ad20628648c8aea51340026c8f0d0cccf
mcallaghan <max.callaghan@pik-potsdam.de>
2026-09-07 15:05:37 +0200

    delete inclusion-specific code and dependencies

--- app/classifiers/__init__.py
+++
@@ -1,9 +0,0 @@
-from .llm import LLMClassifier, PromptConfig, estimate_prompt_tokens
-from .sklearn import SklearnClassifier
-
-__all__ = [
-    "LLMClassifier",
-    "PromptConfig",
-    "SklearnClassifier",
-    "estimate_prompt_tokens",
-]
